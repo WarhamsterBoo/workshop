8c46990b28c705b37a9ddbc19496db70819d4033
Yulia Novikova <y.novikova@dodopizza.com>
2021-02-11 20:46:13 +0300

    add simple endpoint that returns something

---
+++ verysmartapp/src/workshop.verysmartapp.web/Controllers/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace workshop.verysmartapp.web.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        // http://localhost:5000/WeatherForecast/Today?city=Moscow
+        public JsonResult Today(string city)
+        {
+            return Json(new
+            {
+                Date = DateTime.Today,
+                City = city,
+                TemperatureC = -17
+            });
+        }
+    }
+}
--- verysmartapp/src/workshop.verysmartapp.web/Startup.cs
+++ verysmartapp/src/workshop.verysmartapp.web/Startup.cs
@@ -12,13 +12,11 @@ namespace workshop.verysmartapp.web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,7 +28,9 @@ namespace workshop.verysmartapp.web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller}/{action}");
             });
         }
     }
