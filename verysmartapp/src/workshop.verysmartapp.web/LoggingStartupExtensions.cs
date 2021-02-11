e6c0acfcda4f1beccd55a71907b04040f074e799
Yulia Novikova <y.novikova@dodopizza.com>
2021-02-11 21:56:35 +0300

    write logs to elasticsearch

--- verysmartapp/src/workshop.verysmartapp.web/LoggingStartupExtensions.cs
+++ verysmartapp/src/workshop.verysmartapp.web/LoggingStartupExtensions.cs
@@ -1,5 +1,7 @@
+using System;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace workshop.verysmartapp.web
 {
@@ -10,11 +12,20 @@ namespace workshop.verysmartapp.web
                 (hostingContext, loggerConfiguration) =>
                 {
                     var configuration = hostingContext.Configuration;
-
+                    var uri = new Uri(configuration["ElasticSearch:ConnectionString"]);
+                    
+                    var elasticsearchSinkOptions = new ElasticsearchSinkOptions(uri)
+                    {
+                        AutoRegisterTemplate = true,
+                        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                        IndexFormat = "verysmartapp-{0:yyyy.MM}"
+                    };
+                    
                     loggerConfiguration
                         .ReadFrom.Configuration(configuration)
                         .Enrich.FromLogContext()
-                        .WriteTo.Console();
+                        .WriteTo.Console()
+                        .WriteTo.Elasticsearch(elasticsearchSinkOptions);
                 });
     }
 }
